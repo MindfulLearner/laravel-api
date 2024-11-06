4d561dae67cdd2933ba3b88663717d22c78f6944
MindfulLearner <dimajosh1009@gmail.com>
2024-11-06 21:12:00 +0000

    addded get user single id

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -11,10 +11,17 @@ class UserController extends Controller
 {
     public function index()
     {
-        $paginate = Product::paginate(2);
+        $paginate = Product::paginate(3);
         return response()->json([
             'success' => true,
             'products' => $paginate
+        ]);
+    }
+    public function show($id) {
+        $product = Product::find($id);
+        return response()->json([
+            'success' => true,
+            'product' => $product
         ]);
     }
 }
