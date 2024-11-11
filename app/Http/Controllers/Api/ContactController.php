fe772b265d01435796d03e9799676da71aff78df
MindfulLearner <dimajosh1009@gmail.com>
2024-11-11 16:42:47 +0000

    working post

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
     public function createContact(Request $request)
     {
-        dd($request->all());
+
+        $contact = Contact::create($request->all());
+
+        return response()->json($contact, 201);
     }
 }
